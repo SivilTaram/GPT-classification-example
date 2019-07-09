119de23e211f538b2a9eea13d8178475dea75a6d
SivilTaram <qianlxc@outlook.com>
2019-07-09 10:25:34 +0800

    Update script on windows/linux

---
+++ linux_run_script.sh
@@ -0,0 +1,11 @@
+python run_classifier.py \
+--task_name CoLA \
+--do_train \
+--do_eval \
+--data_dir glue_data/CoLA \
+--max_seq_length 128 \
+--train_batch_size 32 \
+--eval_batch_size 8 \
+--learning_rate 2e-5 \
+--num_train_epochs 3.0 \
+--output_dir experiment/CoLA
--- windows_run_script.bat
+++ windows_run_script.bat
@@ -1,5 +1,3 @@
-set CUDA_LAUNCH_BLOCKING=1
-set CUDA_VISIBLE_DEVICES=0
 python run_classifier.py ^
 --task_name CoLA ^
 --do_train ^
@@ -10,4 +8,4 @@ python run_classifier.py ^
 --eval_batch_size 8 ^
 --learning_rate 2e-5 ^
 --num_train_epochs 3.0 ^
---output_dir D:\users\v-qianl\GPTComparsion\experiment\CoLA
+--output_dir experiment\CoLA
